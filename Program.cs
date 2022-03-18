11d91973ec2188126debf00b81498bc66f7cb564
Ajinkya <Ajinkya@DESKTOP-3B7G3J0>
2022-03-18 11:15:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEventDemo
+{
+    //hold method ref -> FailMAsg ,PassMsg
+    public delegate void DisplayMsg();
+    internal class Program
+    {
+
+        static void FailMsg()
+        {
+            Console.WriteLine("Opps...You are Fail");
+        }
+        static void PassMsg()
+        {
+            Console.WriteLine("Congrajulation...Your are Pass");
+
+        }
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.FailEvent += new DisplayMsg(FailMsg);
+            student.PassEvent += new DisplayMsg(PassMsg);
+            student.AcceptMarks(30);
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEventDemo
+{
+    internal class Student
+    {
+        //event 
+        public event DisplayMsg FailEvent; // marks less than 40
+        public event DisplayMsg PassEvent; //marks greater than 40
+
+        public void AcceptMarks(int marks)
+        {
+            if (marks >= 40)
+            {
+                PassEvent(); //call to event or raise an event
+            }
+            else
+            {
+                FailEvent();
+            }
+        }
+    }
+}
